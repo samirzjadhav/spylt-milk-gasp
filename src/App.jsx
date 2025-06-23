068a0cf3668124a8c001b1bf630207e40e18b5aa
samir jadhav <samirzjadhav@gmail.com>
2025-06-23 20:45:58 +0530

    feat: addednew message section on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import gsap from "gsap";
+import MessageSection from "./sections/MessageSection";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <MessageSection />
+      <div className="h-dvh border border-red-500"></div>
     </main>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="fiexed top-0 left-0 z-50 md:p-9 p-3">
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
       <img
         src="./images/nav-logo.svg"
         alt="nav-logo"
